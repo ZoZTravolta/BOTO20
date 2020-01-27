1259324bb24da099e1414bf89ac56aa419ed0137
ZoZTravolta <zoharsaghi@gmail.com>
2020-01-27 14:29:14 +0200

    make it better

---
+++ server.py
@@ -0,0 +1,76 @@
+import json
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+import requests
+from flask_cors import CORS
+# from flask_sqlalchemy import SQLAlchemy
+import string
+import sqlite3
+# from models import Book
+
+
+app = Flask(__name__)
+CORS(app)
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///book_store.db"
+
+# db = SQLAlchemy(app)
+
+
+# class Book(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     book_name = db.Column(db.String(40)) # ,  nullable=False
+#     author = db.Column(db.String(30)) # , unique=True, nullable=False
+#     year = db.Column(db.Integer) # , nullable=False
+#     pic = db.Column(db.String(200), default="https://images-na.ssl-images-amazon.com/images/I/61-uFOBDLDL.jpg") # , nullable=False
+#
+#     def __repr__(self):
+#         return f"Book('{self.book_name}')"
+#
+#
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/message/", methods=['GET'])
+def get_message():
+    user_message = request.args['message']
+    type = request.args['type']
+    # user_message = request.get_json()
+    # print(user_message)
+    if type == 'parrot':
+        return {"message": user_message, "anim": "dog.gif"}
+
+    if type == 'broken':
+        return {"message": "I'm so broken! I'm so broken...", "anim": "heartbroke.gif"}
+
+    if type == 'drunk':
+        return {"message": drunk(), "anim": "dancing.gif"}
+
+    if type == 'trump':
+        return {"message": trump(), "anim": "giggling.gif"}
+
+    else:
+        return {"message": "Hi from zohar's bot", "anim": "confused.gif"}
+
+
+def drunk():
+    connection = sqlite3.connect('botoBrain.db')
+    curs = connection.cursor()
+
+    curs.execute("SELECT * FROM drunk ORDER BY RANDOM() LIMIT 1")
+    row = curs.fetchall()
+
+    return row[0][1]
+
+
+def trump():
+    trumpQ = requests.get(
+        'https://api.whatdoestrumpthink.com/api/v1/quotes/random').json()
+    return trumpQ['message']
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=7000, debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,34 @@
+
+
+
+const sendMessageToServer = async (userMessage, type) => {
+
+  const sendMessage = async () => {
+    try {
+      return await axios.get('http://localhost:7000/message/?message=' + userMessage + '&type=' + type)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
+  const request = await sendMessage()
+
+  if (request.data.message) {
+    document.getElementById('botoPic').setAttribute('src', './static/images/' + request.data.anim)
+    var node = document.createElement("p");
+    var textnode = document.createTextNode(request.data.message);
+    node.appendChild(textnode);
+    document.getElementById("responseText").prepend(node);
+  }
+}
+
+
+
+document.getElementById('btnSubmit').addEventListener('click', () => {
+  const userMessage = document.getElementById('userInput').value;
+  const type = document.getElementById('selectType').value
+
+  sendMessageToServer(userMessage, type)
+})
+
